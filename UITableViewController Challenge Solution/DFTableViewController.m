66db5caf398ed195642fb03d63c3bfe228bcd1cc
dsfank <dsfank@live.com>
2014-07-21 22:41:45 -0500

    updated with extra credit

--- UITableViewController Challenge Solution/DFTableViewController.m
+++ UITableViewController Challenge Solution/DFTableViewController.m
@@ -77,14 +77,17 @@
     if (indexPath.section == 0)
     {
         cell.textLabel.text = @"I am in section 0";
+        cell.textLabel.textColor = [UIColor redColor];
     }
     else if (indexPath.section == 1)
     {
         cell.textLabel.text = @"another section";
+        cell.textLabel.textColor = [UIColor blueColor];
     }
     else
     {
         cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row];
+        cell.textLabel.textColor = [UIColor yellowColor];
     }
     
     return cell;
